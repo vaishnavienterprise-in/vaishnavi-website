f44691e56b4b62e17402ce00ba99de1d7e5836bf
vaishnavienterprise-in <vaishnavienterprise.print@gmail.com>
2026-05-04 19:06:07 +0530

    Add sitemap for SEO

---
+++ app/sitemap.ts
@@ -0,0 +1,20 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://vaishnavienterprise.in',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://vaishnavienterprise.in/barcode-labels',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://vaishnavienterprise.in/roll-labels',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://vaishnavienterprise.in/hologram-labels',
+      lastModified: new Date(),
+    },
+  ];
+}
