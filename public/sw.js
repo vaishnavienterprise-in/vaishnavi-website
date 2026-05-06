12c83f0076bbf715302d45c3040fd37e54147451
vaishnavienterprise-in <vaishnavienterprise.print@gmail.com>
2026-05-06 11:29:48 +0530

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = "vaishnavi-v1";
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        "/",
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => {
+      return res || fetch(event.request);
+    })
+  );
+});
