9e280ee2644eb43d58a710378aeb58a1e57f637a
vaishnavienterprise-in <vaishnavienterprise.print@gmail.com>
2026-05-07 11:18:29 +0530

    Add product master database

---
+++ app/data/products.ts
@@ -0,0 +1,51 @@
+export const products = [
+  {
+    id: 1,
+    name: "Chromo Label",
+    material: "Chromo",
+    hsn: "48211020",
+    gst: 18,
+    unit: "Nos",
+    datasheet: "/datasheets/chromo.pdf",
+  },
+
+  {
+    id: 2,
+    name: "White PP Label",
+    material: "White PP",
+    hsn: "48211020",
+    gst: 18,
+    unit: "Nos",
+    datasheet: "/datasheets/white-pp.pdf",
+  },
+
+  {
+    id: 3,
+    name: "Silver PP Label",
+    material: "Silver PP",
+    hsn: "48211020",
+    gst: 18,
+    unit: "Nos",
+    datasheet: "/datasheets/silver-pp.pdf",
+  },
+
+  {
+    id: 4,
+    name: "Hologram Label",
+    material: "Hologram",
+    hsn: "48211020",
+    gst: 18,
+    unit: "Nos",
+    datasheet: "/datasheets/hologram.pdf",
+  },
+
+  {
+    id: 5,
+    name: "Barcode Label",
+    material: "Barcode",
+    hsn: "48211020",
+    gst: 18,
+    unit: "Nos",
+    datasheet: "/datasheets/barcode.pdf",
+  },
+];
